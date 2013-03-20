07887a05429c61866349d56a394bec31d5384356
Lee Reilly <lee@leereilly.net>
2013-03-20 13:11:28 -0700

    Add IP-specific fields to IP Record

--- lib/demandbase/ip_record.rb
+++ lib/demandbase/ip_record.rb
@@ -1,5 +1,12 @@
 module Demandbase
   class IPRecord < Demandbase::Record
-
+    attr_accessor :ip
+    attr_accessor :audience
+    attr_accessor :audience_segment
+    attr_accessor :information_level
+    attr_accessor :worldhq_sid
+    attr_accessor :domestichq_sid
+    attr_accessor :hq_sid
+    attr_accessor :isp
   end
 end
